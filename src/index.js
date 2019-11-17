2aeba50fbca3a9df01f8fadac4811bdf8380c5a3
Antti Järveläinen <51124276+aojarv@users.noreply.github.com>
2019-11-17 23:37:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,69 +3,42 @@ import ReactDOM from 'react-dom'
 import Axios from 'axios'
 import './index.css'
 
-
-
 const App = () => {
 
     const [data, setData] = useState("")
-    const [title0, setTitle0] = useState("")
-    const [abstract0, setAbstract0] = useState("")
-    const [title1, setTitle1] = useState("")
-    const [abstract1, setAbstract1] = useState("")
-    const [title2, setTitle2] = useState("")
-    const [abstract2, setAbstract2] = useState("")
-    const [title3, setTitle3] = useState("")
-    const [abstract3, setAbstract3] = useState("")
-    const [title4, setTitle4] = useState("")
-    const [abstract4, setAbstract4] = useState("")
-    const [title5, setTitle5] = useState("")
-    const [abstract5, setAbstract5] = useState("")
-    const [title6, setTitle6] = useState("")
-    const [abstract6, setAbstract6] = useState("")
-    const [title7, setTitle7] = useState("")
-    const [abstract7, setAbstract7] = useState("")
-    const [title8, setTitle8] = useState("")
-    const [abstract8, setAbstract8] = useState("")
-    const [title9, setTitle9] = useState("")
-    const [abstract9, setAbstract9] = useState("")
-    const [title10, setTitle10] = useState("")
-    const [abstract10, setAbstract10] = useState("")
+    const [titles, setTitles] = useState([])
+    const [abstracts, setAbstracts] = useState([])
 
     const getData = () => {
         console.log(data)
-        console.log(data[0].title)
-        console.log(title0)
-        console.log(abstract0)
-        console.log(title1)
-        console.log(abstract1)
+        console.log(titles)
+        console.log(abstracts)
     }
 
     useEffect(() => {
         const URL = `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=xGsgXlF7e8OzGsfLXyAWazfAZBhG4IA3`
         Axios.get(URL).then(response => {
+            setTitles(titles => [...titles, response.data.results[0].title, response.data.results[1].title, 
+                                            response.data.results[2].title, response.data.results[3].title, 
+                                            response.data.results[4].title, response.data.results[5].title,
+                                            response.data.results[6].title, response.data.results[7].title,
+                                            response.data.results[8].title, response.data.results[9].title,
+                                            response.data.results[10].title, response.data.results[11].title,
+                                            response.data.results[12].title, response.data.results[13].title,
+                                            response.data.results[14].title, response.data.results[15].title,
+                                            response.data.results[16].title, response.data.results[17].title,
+                                            response.data.results[18].title, response.data.results[19].title,])  
+            setAbstracts(abstracts => [...abstracts, response.data.results[0].abstract, response.data.results[1].abstract,
+                                            response.data.results[2].abstract, response.data.results[3].abstract,
+                                            response.data.results[4].abstract, response.data.results[5].abstract,
+                                            response.data.results[6].abstract, response.data.results[7].abstract,
+                                            response.data.results[8].abstract, response.data.results[9].abstract,
+                                            response.data.results[10].abstract, response.data.results[11].abstract,
+                                            response.data.results[12].abstract, response.data.results[13].abstract,
+                                            response.data.results[14].abstract, response.data.results[15].abstract,
+                                            response.data.results[16].abstract, response.data.results[17].abstract,
+                                            response.data.results[18].abstract, response.data.results[19].abstract,])
             setData(response.data.results)
-            setTitle0(response.data.results[0].title)
-            setAbstract0(response.data.results[0].abstract)
-            setTitle1(response.data.results[1].title)
-            setAbstract1(response.data.results[1].abstract)
-            setTitle2(response.data.results[2].title)
-            setAbstract2(response.data.results[2].abstract)
-            setTitle3(response.data.results[3].title)
-            setAbstract3(response.data.results[3].abstract)
-            setTitle4(response.data.results[4].title)
-            setAbstract4(response.data.results[4].abstract)
-            setTitle5(response.data.results[5].title)
-            setAbstract5(response.data.results[5].abstract)
-            setTitle6(response.data.results[6].title)
-            setAbstract6(response.data.results[6].abstract)
-            setTitle7(response.data.results[7].title)
-            setAbstract7(response.data.results[7].abstract)
-            setTitle8(response.data.results[8].title)
-            setAbstract8(response.data.results[8].abstract)
-            setTitle9(response.data.results[9].title)
-            setAbstract9(response.data.results[9].abstract)
-            setTitle10(response.data.results[10].title)
-            setAbstract10(response.data.results[10].abstract)
         })
         
     }, [])
@@ -85,17 +58,27 @@ const App = () => {
 
     return(
         <>
-        <New title={title0} abstract={abstract0}/>
-        <New title={title1} abstract={abstract1}/>
-        <New title={title2} abstract={abstract2}/> 
-        <New title={title3} abstract={abstract3}/> 
-        <New title={title4} abstract={abstract4}/> 
-        <New title={title5} abstract={abstract5}/> 
-        <New title={title6} abstract={abstract6}/> 
-        <New title={title7} abstract={abstract7}/> 
-        <New title={title8} abstract={abstract8}/> 
-        <New title={title9} abstract={abstract9}/> 
-        <New title={title10} abstract={abstract10}/>       
+        <New title={titles[0]} abstract={abstracts[0]}/>
+        <New title={titles[1]} abstract={abstracts[1]}/>   
+        <New title={titles[2]} abstract={abstracts[2]}/>   
+        <New title={titles[3]} abstract={abstracts[3]}/>   
+        <New title={titles[4]} abstract={abstracts[4]}/>   
+        <New title={titles[5]} abstract={abstracts[5]}/>   
+        <New title={titles[6]} abstract={abstracts[6]}/>   
+        <New title={titles[7]} abstract={abstracts[7]}/>   
+        <New title={titles[8]} abstract={abstracts[8]}/>   
+        <New title={titles[9]} abstract={abstracts[9]}/>   
+        <New title={titles[10]} abstract={abstracts[10]}/>         
+        <New title={titles[11]} abstract={abstracts[11]}/>   
+        <New title={titles[12]} abstract={abstracts[12]}/>   
+        <New title={titles[13]} abstract={abstracts[13]}/>   
+        <New title={titles[14]} abstract={abstracts[14]}/>   
+        <New title={titles[15]} abstract={abstracts[15]}/>   
+        <New title={titles[16]} abstract={abstracts[16]}/>   
+        <New title={titles[17]} abstract={abstracts[17]}/>   
+        <New title={titles[18]} abstract={abstracts[18]}/>   
+        <New title={titles[19]} abstract={abstracts[19]}/>   
+
         
         <button onClick={getData}>xd</button>
         </>
