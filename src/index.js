6fd5a11e4b735ede424e5fffd726bc33f320d5f8
Antti Järveläinen <51124276+aojarv@users.noreply.github.com>
2019-11-18 12:07:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,38 +6,27 @@ import './index.css'
 const App = () => {
 
     const [data, setData] = useState("")
-    const [titles, setTitles] = useState([])
-    const [abstracts, setAbstracts] = useState([])
+    const [news, setNews] = useState([])
 
     const getData = () => {
         console.log(data)
-        console.log(titles)
-        console.log(abstracts)
+        console.log(news)
     }
 
     useEffect(() => {
         const URL = `https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=xGsgXlF7e8OzGsfLXyAWazfAZBhG4IA3`
         Axios.get(URL).then(response => {
-            setTitles(titles => [...titles, response.data.results[0].title, response.data.results[1].title, 
-                                            response.data.results[2].title, response.data.results[3].title, 
-                                            response.data.results[4].title, response.data.results[5].title,
-                                            response.data.results[6].title, response.data.results[7].title,
-                                            response.data.results[8].title, response.data.results[9].title,
-                                            response.data.results[10].title, response.data.results[11].title,
-                                            response.data.results[12].title, response.data.results[13].title,
-                                            response.data.results[14].title, response.data.results[15].title,
-                                            response.data.results[16].title, response.data.results[17].title,
-                                            response.data.results[18].title, response.data.results[19].title,])  
-            setAbstracts(abstracts => [...abstracts, response.data.results[0].abstract, response.data.results[1].abstract,
-                                            response.data.results[2].abstract, response.data.results[3].abstract,
-                                            response.data.results[4].abstract, response.data.results[5].abstract,
-                                            response.data.results[6].abstract, response.data.results[7].abstract,
-                                            response.data.results[8].abstract, response.data.results[9].abstract,
-                                            response.data.results[10].abstract, response.data.results[11].abstract,
-                                            response.data.results[12].abstract, response.data.results[13].abstract,
-                                            response.data.results[14].abstract, response.data.results[15].abstract,
-                                            response.data.results[16].abstract, response.data.results[17].abstract,
-                                            response.data.results[18].abstract, response.data.results[19].abstract,])
+          const arr = []
+
+            for(let i = 0; i < response.data.results.length; i++){
+              let singleObject = {
+                title: response.data.results[i].title,
+                abstract: response.data.results[i].abstract
+              }
+              arr.push(singleObject)
+            }
+            
+            setNews(arr)
             setData(response.data.results)
         })
         
@@ -56,30 +45,22 @@ const App = () => {
         )
     }
 
+    const Map = () => {
+      if(news.length === 0){
+        return null;
+      }
+      else{
+        return(
+          <>
+          {news.map(item => <New title={item.title} abstract={item.abstract}/>)}
+          </>
+        )
+      }
+    }
+
     return(
         <>
-        <New title={titles[0]} abstract={abstracts[0]}/>
-        <New title={titles[1]} abstract={abstracts[1]}/>   
-        <New title={titles[2]} abstract={abstracts[2]}/>   
-        <New title={titles[3]} abstract={abstracts[3]}/>   
-        <New title={titles[4]} abstract={abstracts[4]}/>   
-        <New title={titles[5]} abstract={abstracts[5]}/>   
-        <New title={titles[6]} abstract={abstracts[6]}/>   
-        <New title={titles[7]} abstract={abstracts[7]}/>   
-        <New title={titles[8]} abstract={abstracts[8]}/>   
-        <New title={titles[9]} abstract={abstracts[9]}/>   
-        <New title={titles[10]} abstract={abstracts[10]}/>         
-        <New title={titles[11]} abstract={abstracts[11]}/>   
-        <New title={titles[12]} abstract={abstracts[12]}/>   
-        <New title={titles[13]} abstract={abstracts[13]}/>   
-        <New title={titles[14]} abstract={abstracts[14]}/>   
-        <New title={titles[15]} abstract={abstracts[15]}/>   
-        <New title={titles[16]} abstract={abstracts[16]}/>   
-        <New title={titles[17]} abstract={abstracts[17]}/>   
-        <New title={titles[18]} abstract={abstracts[18]}/>   
-        <New title={titles[19]} abstract={abstracts[19]}/>   
-
-        
+        <Map/>
         <button onClick={getData}>xd</button>
         </>
     )
