9714fedfc5217fc6e09d2fdfd04fa47e33eeb25e
Antti Järveläinen <51124276+aojarv@users.noreply.github.com>
2019-12-23 19:22:36 +0200

    Add files via upload

---
+++ build/precache-manifest.45c68f6b4c0d8d40f6bcb37969c507f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9d0873e30790ff8fb2e2749c095b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb9f2c1b394a0b09198",
+    "url": "/static/css/main.10c623db.chunk.css"
+  },
+  {
+    "revision": "1bc36f1b80858de2f581",
+    "url": "/static/js/2.37bb4278.chunk.js"
+  },
+  {
+    "revision": "eeb9f2c1b394a0b09198",
+    "url": "/static/js/main.57441fdd.chunk.js"
+  },
+  {
+    "revision": "1c2353939ed7c45c3d69",
+    "url": "/static/js/runtime-main.582519fa.js"
+  }
+]);
